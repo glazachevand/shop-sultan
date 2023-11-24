8685e42ff7c9e3ba966c9cc695a9e3d8ff439407
glazachevand <111851786+glazachevand@users.noreply.github.com>
2023-11-24 16:01:10 +0700

    correct checkboxes

--- src/components/UI/Checkbox/Checkbox.tsx
+++ src/components/UI/Checkbox/Checkbox.tsx
@@ -10,13 +10,18 @@ interface CheckboxProps {
   onChange?: () => void;
 }
 
+const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+
+};
+
 export const Checkbox = (props: CheckboxProps) => {
   const { className, item, count, checked, onChange } = props;
   return (
     <label className={classNames(cls.checkbox, {}, [className])} data-testid="checkbox">
-      <input className={cls.input} type="checkbox" value={item} name={item}
-        checked={checked} onChange={onChange} />{item}
+      {item}
       <span className={cls.count}> ({count})</span>
+      <input className={cls.input} type="checkbox" value={item} name={item} checked={checked} onChange={onChangeHandler} />
+      <span className={`${cls.checkmark} ${checked ? cls.active : ''}`}></span>
     </label>
   );
 };
